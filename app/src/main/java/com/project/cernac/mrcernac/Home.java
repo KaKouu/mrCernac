a56f08615b49a78ae62277859469836ef1dedf53
Eliott BALDY <kakou8700@gmail.com>
2019-02-17 19:29:58 +0100

    changement du theme et du nom de l'application

--- app/src/main/java/com/project/cernac/mrcernac/Home.java
+++ app/src/main/java/com/project/cernac/mrcernac/Home.java
@@ -1,7 +1,7 @@
 package com.project.cernac.mrcernac;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +9,6 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
     }
 }
