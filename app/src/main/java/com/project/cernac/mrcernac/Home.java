e83f19aa074e9b07816068aaef19b8faa9016b56
Eliott BALDY <kakou8700@gmail.com>
2019-02-18 19:32:30 +0100

    ajout d'une classe pour les Preferences de l'app + jeux d'essai sur la page home

--- app/src/main/java/com/project/cernac/mrcernac/Home.java
+++ app/src/main/java/com/project/cernac/mrcernac/Home.java
@@ -2,13 +2,55 @@ package com.project.cernac.mrcernac;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.project.cernac.mrcernac.utils.Prefs;
+
+import static java.lang.String.valueOf;
 
 public class Home extends AppCompatActivity {
+
+    private Prefs prefs = null;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        prefs = new Prefs(this);
+        final int[] actualLevels = {prefs.getLevel()};
+
+        TextView testPrefs = findViewById(R.id.prefTest);
+        Button addBt = findViewById(R.id.addBt);
+
+        System.out.println("Actual level is " + actualLevels[0]);
+
+        testPrefs.setText(valueOf(actualLevels[0]));
+
+        addBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualLevels[0] +=1;
+                prefs.setLevel(actualLevels[0]);
+                System.out.println("Actual level is " + actualLevels[0]);
+                System.out.println("Actual level from prefs is " + prefs.getLevel());
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
 
     }
 }
---
+++ app/src/main/java/com/project/cernac/mrcernac/utils/Prefs.java
@@ -0,0 +1,42 @@
+package com.project.cernac.mrcernac.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Prefs {
+
+
+    private Context context;
+
+    private String PREFS_NAME;
+
+    private SharedPreferences settings;
+    private SharedPreferences.Editor editor ;
+
+    private String ACTUAL_LEVEL;
+
+    private int level;
+
+    public Prefs(Context context) {
+        this.context = context;
+        this.PREFS_NAME = "com.project.cernac.mrcernac";
+        this.ACTUAL_LEVEL = "actual_level";
+        this.settings = context.getSharedPreferences(PREFS_NAME, 0);
+        this.editor = settings.edit();
+    }
+
+
+
+
+    public void setLevel(int level){
+        this.editor.putInt(ACTUAL_LEVEL, level);
+        editor.commit();
+    }
+
+    public int getLevel(){
+        return this.settings.getInt(ACTUAL_LEVEL,0);
+    }
+
+
+
+}
