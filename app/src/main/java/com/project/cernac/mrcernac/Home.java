c4b27ff737fab4234080f50f789bd81b38fa8d9e
Eliott BALDY <kakou8700@gmail.com>
2019-02-19 09:28:28 +0100

    preparation pour le commencement du projet

--- app/src/main/java/com/project/cernac/mrcernac/Home.java
+++ app/src/main/java/com/project/cernac/mrcernac/Home.java
@@ -1,15 +1,10 @@
 package com.project.cernac.mrcernac;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.Button;
-import android.widget.MediaController;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.project.cernac.mrcernac.utils.Prefs;
 
@@ -24,31 +19,18 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        prefs = new Prefs(this);
-        final int[] actualLevels = {prefs.getLevel()};
 
-        TextView testPrefs = findViewById(R.id.prefTest);
-        Button addBt = findViewById(R.id.addBt);
 
-        System.out.println("Actual level is " + actualLevels[0]);
-
-        testPrefs.setText(valueOf(actualLevels[0]));
-
-        addBt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                actualLevels[0] +=1;
-                prefs.setLevel(actualLevels[0]);
-                System.out.println("Actual level is " + actualLevels[0]);
-                System.out.println("Actual level from prefs is " + prefs.getLevel());
-            }
-        });
 
 
 
     }
 }
 
+
+/* ///////////////PREFS//////////////////
+prefs = new Prefs(this);
+final int[] actualLevels = {prefs.getLevel()};
 
 /* /////////////// WEBVIEWS//////////////////
         WebView webTest = findViewById(R.id.webTest);
