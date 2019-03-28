63ed1946e0f1605fe4e4f7874d690122f564cf12
Eliott BALDY <kakou8700@gmail.com>
2019-03-28 15:54:59 +0100

    ajout niveau du volume

---
+++ app/src/main/java/com/project/cernac/mrcernac/SoundLevel.java
@@ -0,0 +1,56 @@
+package com.project.cernac.mrcernac;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.ImageView;
+
+import com.project.cernac.mrcernac.utils.Level;
+
+public class SoundLevel extends AppCompatActivity {
+
+    Level levelX = new Level(this);
+    int rep=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sound_level);
+
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent intent = new Intent(SoundLevel.this, Home.class);
+            startActivity(intent);
+            return false;
+        } else if(keyCode == KeyEvent.KEYCODE_VOLUME_DOWN){
+            picDown();
+            return false;
+
+        } else {
+                return super.onKeyDown(keyCode, event);
+            }
+
+    }
+
+
+    public void picDown(){
+        ImageView coco = findViewById(R.id.coco);
+        coco.setY(coco.getY()+10);
+
+        if (rep>=100){
+            levelX.finish();
+            Intent intent = new Intent(SoundLevel.this, Home.class);
+            startActivity(intent);
+        }
+        rep+=1;
+        System.out.println(rep);
+    }
+
+}
